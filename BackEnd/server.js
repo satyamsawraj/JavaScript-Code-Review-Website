b000e2724352db71d8dad06373eb9ecfa29f4c55
satyamsawraj <satyamsawraj4@gmail.com>
2025-08-20 22:26:54 +0530

    Add Vercel configuration for monorepo

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,8 +1,10 @@
-require('dotenv').config()
-const app = require('./src/app')
+require('dotenv').config();
+const app = require('./src/app');
+const cors = require('cors'); // Import the cors middleware
 
-
+// Use the cors middleware to enable CORS for all requests
+app.use(cors()); 
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-})
+    console.log('Server is running on http://localhost:3000');
+});
