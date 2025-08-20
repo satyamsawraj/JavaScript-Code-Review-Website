2d4122029e42164856b9706207d76ffa1269e026
satyamsawraj <satyamsawraj4@gmail.com>
2025-08-21 01:17:11 +0530

    Fix: Export server app for Vercel

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,7 @@
 require('dotenv').config();
 const app = require('./src/app');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
 
-// Use the cors middleware to enable CORS for all requests
-app.use(cors()); 
+app.use(cors());
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
+module.exports = app;
